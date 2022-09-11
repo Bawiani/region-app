455e90451fdf963ad15a85928d26a41958e3e0be
Mohammed Abudu <mbawiani33@gmail.com>
2022-09-11 10:33:43 +0000

    Remove server directory

--- server/src/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err) => {
-    if (!err) {
-        console.log('Connection successful!');
-    } else {
-        console.log('Connection unsuccessful!');
-    }
-});
